696713f21010c8de275be9f79518baee7847b170
Cláudio Santos <claudio@sistnet.com.br>
2023-12-06 23:15:36 -0300

    functions

---
+++ lib/01_funcoes/01_funcoes.dart
@@ -0,0 +1,10 @@
+void main() {
+  final valorCalculado = soma(10, 30);
+
+  print("O total da soma é $valorCalculado");
+}
+
+int soma(int valor1, int valor2) {
+  print("Soma de dois numeros ($valor1, $valor2)");
+  return valor1 + valor2;
+}
